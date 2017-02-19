abd431a5f570772e51e6d6e6a9fe5c6110662562
Hao Xiangpeng <3150102255@zju.edu.cn>
2017-02-19 09:28:51 +0800

    finished input validate

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -13,24 +13,24 @@
         <el-form  ref="form" :model="form">
           <el-form-item>
             <div class="group">      
-                <input v-model="form.name" type="text" required>
-                <label>用户名/邮箱</label>
+                <input v-model="form.phone" type="text" required>
+                <label v-bind:class="{error: userInput[0].error}">{{userInput[0].msg}}</label>
             </div>
           </el-form-item>
           <el-form-item>
             <div class="group">      
                 <input  type="password" v-model="form.password" required>
-                <label>密码</label>
+                <label v-bind:class="{error: userInput[1].error}">{{userInput[1].msg}}</label>
             </div>
           </el-form-item>
           <el-form-item>
             <div class="forget">忘记密码?</div>
           </el-form-item>
           <el-form-item>
-            <button class="btn  gray" type="button"><span>登录</span></button>
+            <button @click="login" class="btn gray" type="button"><span>登录</span></button>
           </el-form-item>
           <el-form-item>
-            <button class="btn" type="button"><span>注册</span></button>
+            <button @click="toSignup" class="btn" type="button"><span>注册</span></button>
           </el-form-item>
           <el-form-item>
             <div class="notice" @click="toHome">随便逛逛?</div>
@@ -50,23 +50,66 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       form: {
-        name: '',
+        phone: '',
         password: ''
       }
     }
   },
-  // props: {
-  //   section: {
-  //     type: String,
-  //     default: ''
-  //   }
-  // },
+  computed: {
+    userInput: function () {
+      let result = []
+      if (this.form.phone.length > 0 && (!this.form.phone.match(/\d/g) || this.form.phone.match(/\d/g).length !== 11)) {
+        result.push({
+          msg: '手机号不对噢',
+          error: true,
+          blank: false
+        })
+      } else if (this.form.phone.length === 0) {
+        result.push({
+          msg: '手机号',
+          error: false,
+          blank: true
+        })
+      } else {
+        result.push({
+          msg: '手机号',
+          error: false,
+          blank: false
+        })
+      }
+      if (this.form.password.length > 0 && this.form.password.length < 7 || this.form.password.length > 30) {
+        result.push({
+          msg: '密码长度不够',
+          error: true,
+          blank: false
+        })
+      } else if (this.form.password.length === 0) {
+        result.push({
+          msg: '密码',
+          error: false,
+          blank: true
+        })
+      } else {
+        result.push({
+          msg: '密码',
+          error: false,
+          blank: false
+        })
+      }
+      return result
+    }
+  },
   methods: {
     onSubmit () {
       console.log('submit')
     },
     toHome: function () {
       this.$router.push('/')
+    },
+    toSignup: function () {
+      this.$router.push('/signup')
+    },
+    login: function () {
     }
   }
 }
@@ -77,6 +120,9 @@ export default {
 </style>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error{
+  color: #e74c3c !important;
+}
 h1, h2 {
   font-weight: normal;
 }
