563af41680a8263e0b59e9b87097fa0d0c022c47
Hao Xiangpeng <3150102255@zju.edu.cn>
2017-02-26 19:59:52 +0800

    add small feature

--- src/components/PersonProject.vue
+++ src/components/PersonProject.vue
@@ -1,14 +1,14 @@
 <template>
   <div style="height: 100%;margin: 0.5rem 2rem 5rem 2rem; overflow: auto; padding-bottom: 100px">
     <el-form >
-      <el-form-item label="姓名">
+      <el-form-item label="真实姓名">
         <el-input v-model="form.name"></el-input>
       </el-form-item>
       <el-form-item label="学科">
         <el-input v-model="form.area"></el-input>          
       </el-form-item>
       <el-form-item label="描述">
-        <el-input type="textarea" v-model="form.desc"></el-input>          
+        <el-input type="textarea" v-model="form.description"></el-input>          
       </el-form-item>
       <el-form-item style="margin-top: 0.5rem;">
         <el-button type="primary" @click="onSubmit">发布活动</el-button>
@@ -18,8 +18,16 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {mapGetters} from 'vuex'
 export default {
   name: 'hello',
+  computed: {
+    ...mapGetters([
+      'postProjectURL',
+      'superToken'
+    ])
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
@@ -35,7 +43,16 @@ export default {
   },
   methods: {
     onSubmit: function () {
-      console.log('you click')
+      let self = this
+
+      axios.post(self.postProjectURL, {
+        'teacher': self.form,
+        'token': self.superToken
+      }).then(
+        response => {
+          console.log(response)
+        }
+      )
     }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,9 @@ Vue.use(Vuex)
 const state = {
   login: false,
   showLoginModal: false,
-  userInfo: ''
+  userInfo: '',
+  postProjectURL: 'https://api.houaa.xyz/index.php/api/teacher',
+  superToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9ub3NzbC5ob3VhYS54eXoiLCJpcCI6IjEyNy4wLjAuMSIsImlkIjoiaGFvaGFvIiwicGhvbmUiOiIxMjM0NTY3ODkwMSIsImlhdCI6MTQ4ODEwNTkxOH0.2r47M-_GmiH591q4Yscp-Hbp8eWqN0k7eKlANlAtfg4'
 }
 
 const mutations = {
@@ -35,7 +37,9 @@ const mutations = {
 const getters = {
   loggedIn: state => state.login,
   loginModal: state => state.showLoginModal,
-  userInfo: state => state.userInfo
+  userInfo: state => state.userInfo,
+  postProjectURL: state => state.postProjectURL,
+  superToken: state => state.superToken
 }
 
 export default new Vuex.Store({
