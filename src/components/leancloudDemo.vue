8b95080b1f37295e8d8dbd26c2134ad972f4be4f
haoxiangpeng <3150102255@zju.edu.cn>
2017-05-17 17:03:18 +0800

    leancloud sample

--- src/components/leancloudDemo.vue
+++ src/components/leancloudDemo.vue
@@ -13,7 +13,16 @@
     <el-input placeholder="recaptcha" v-model="recaptcha"></el-input>
     <el-button @click="sendSMS">Send message</el-button>
     <el-button @click="signInPhone">test sign in with phone</el-button>
-  
+    <hr>
+    <el-input placeholder="User Name" v-model="username"></el-input>
+    <el-input placeholder="Password" v-model="password"></el-input>
+    <el-input placeholder="Email" v-model="email"></el-input>
+    <el-button @click="testAddUser">add user</el-button>
+    <el-button @click="query">query</el-button>
+    <el-card v-for="(item, index) in queryResult" :key="index">
+      用户名:{{item.attributes.name}},密码:{{item.attributes.password}},邮箱:{{item.attributes.email}} 生成日期:{{item.createdAt}}
+    </el-card>
+    <!--<div v-for="item in queryResult">{{item}}</div>-->
   </div>
 </template>
 
@@ -27,7 +36,7 @@ AV.init({
 })
 export default {
   name: 'leancloud',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App',
       username: '',
@@ -36,10 +45,34 @@ export default {
       loginPwd: '',
       loginName: '',
       phone: '',
-      recaptcha: ''
+      recaptcha: '',
+      queryResult: []
     }
   },
   methods: {
+    testAddUser: function () {
+      let self = this
+      var UserList = AV.Object.extend('UserList')
+      var testObject = new UserList()
+      testObject.save({
+        name: self.username,
+        password: self.password,
+        email: self.email
+      }).then(object => {
+        self.$message('添加成功')
+        console.log(object)
+      })
+    },
+    query: function () {
+      let self = this
+      let query = new AV.Query('UserList')
+      query.find().then(result => {
+        self.queryResult = result
+        console.log(result)
+      }, error => {
+        console.log(error)
+      })
+    },
     signup: function () {
       let user = new AV.User()
       let self = this
@@ -78,6 +111,19 @@ export default {
         self.$message(loggedInUser.attributes.email)
       }, error => {
         console.log(error)
+      })
+    },
+    wechat: function () {
+      AV.User.signUpOrlogInWithAuthData({
+        // 微博（weibo）用 uid
+        // 微信（weixin）和 QQ（qq）用 openid
+        'openid': 'oPrJ7uM5Y5oeypd0fyqQcKCaRv3o',
+        'access_token': 'OezXcEiiBSKSxW0eoylIeNFI3H7HsmxM7dUj1dGRl2dXJOeIIwD4RTW7Iy2IfJePh6jj7OIs1GwzG1zPn7XY_xYdFYvISeusn4zfU06NiA1_yhzhjc408edspwRpuFSqtYk0rrfJAcZgGBWGRp7wmA',
+        'expires_at': '2017-06-06T11:43:11.904Z'
+      }, 'weixin').then(function (s) {
+        console.log(s)
+      }, function (e) {
+        console.log(e)
       })
     }
   }
