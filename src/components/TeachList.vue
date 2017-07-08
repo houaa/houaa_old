2ddbd055cc80e630915716b3ebb7b4f016e9c398
haoxiangpeng <3150102255@zju.edu.cn>
2017-07-08 14:48:49 +0800

    change homepage view

--- src/components/TeachList.vue
+++ src/components/TeachList.vue
@@ -3,8 +3,11 @@
   
     <el-row :gutter="12">
       <el-col class="main-card" :xs="24" :sm="8" :lg="6" :key="index" v-for="(user,index) in allUsers" v-on:click.native="showDetail(index, $event)">
-        <div style="width:25%;display:flex;justify-content: center;align-items: center;">
-          <img width="60%;" style="" src="../assets/houaa-r.png">
+        <div style="width:25%;display:flex;justify-content: center;
+                    align-items: center;font-size:2em;color:#00AF63;text-align:center;">
+          <!--<img width="60%;" style="" src="../assets/houaa-r.png">-->
+  
+          {{user.attributes.name[0]}}
         </div>
         <div style="flex-grow:1;margin: auto 0.5rem auto 0.5rem;padding: 0.6rem 1.5rem 0.6rem 0rem;border-bottom:1px solid #eee;">
           <div style="padding:0.5rem 0 0.5rem 0;">
@@ -12,13 +15,17 @@
             <span style="color:#555;"> /{{user.attributes.selfIntro}}</span>
           </div>
           <div style="font-size: 14px; color: #555">
-            <div>年级：{{user.attributes.grade}}</div>
+            <div>{{user.attributes.grade}}</div>
             <div>
-              <el-tag v-for="(tag,index) in user.attributes.tags" key="index" type="success">{{tag}}</el-tag>
+              <el-tag v-for="(tag,index) in user.attributes.tags" :key="index" type="success">{{tag}}</el-tag>
             </div>
+            <el-input :value="testData" @change="handleValue">{{testData2}}</el-input>
             <div>加入时间：{{user.createdAt|toDate}}</div>
             <!--<el-button v-on:click="showDetail(index, $event)" type="text" class="button">了解详情</el-button>-->
           </div>
+        </div>
+        <div>
+          <el-button>立刻预约</el-button>
         </div>
       </el-col>
     </el-row>
@@ -43,6 +50,9 @@ export default {
     toInt: function (value) {
       return parseInt(value)
     },
+    numberOnly: function (value) {
+      return value.toString(0)
+    },
     toDate: function (value) {
       console.log(value)
       let newDate = new Date(value)
@@ -53,7 +63,9 @@ export default {
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
-      allUsers: {}
+      allUsers: {},
+      testData: '',
+      testData2: ''
     }
   },
   created: function () {
@@ -67,6 +79,8 @@ export default {
       'setCurrentTeacher',
       'setAllTeachers'
     ]),
+    handleValue: function (value) {
+    },
     showDetail: function (index, event) {
       if (!this.loggedIn) {
         this.showLoginModal()
