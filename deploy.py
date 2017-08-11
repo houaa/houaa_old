489eb2c04f6f4d20f61ac5e290397f738a6314ef
haoxiangpeng <3150102255@zju.edu.cn>
2017-08-12 00:00:54 +0800

    change deploy

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ call(['npm', 'run', 'build'])
 
 # generate the service worker from static/sw.js
 # and output to the dist/sw.js(root folder)
-# Update the cache name for each deployment,
+# Update the cache name to the newest version,
 # see https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
 now = datetime.now()
 with open('static/sw.js', 'r+') as f:
